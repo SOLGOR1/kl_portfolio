4c5155b53f1c9125c36982925deb13f421f43b14
SOLGOR1 <info.sorgol@proton.me>
2024-12-18 18:24:39 +0100

    prettier

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -116,11 +116,11 @@ const Sidebar = () => {
             Because nothing says
             <Text variant="emphasis" as="span">
               {' '}
-            unmatched power, digital supremacy, and the future of humanity
+              unmatched power, digital supremacy, and the future of humanity
             </Text>
             <br />
-            like a Leek with a mission. 🌱 Its the meme revolution you never saw coming
-            and now, youre a part of it. Welcome to the new era.
+            like a Leek with a mission. 🌱 Its the meme revolution you never saw
+            coming and now, youre a part of it. Welcome to the new era.
           </MotionText>
 
           <MotionButton
